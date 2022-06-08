9d44dd61d5fbcca050b5482ad861d1b2bed1b4ca
Ludovic Muller <ludovicmuller1@gmail.com>
2022-06-09 00:07:23 +0200

    fix env + add OIDC auth

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,27 +1,27 @@
-import env from "react-dotenv";
+import { OidcSecure } from '@axa-fr/react-oidc';
 import Header from './components/Header';
 import './App.css';
 
-const GitLabInstance = env.GITLAB
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <OidcSecure>
+      <div className="App">
+        <Header />
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </OidcSecure>
   );
 }
 
--- react/src/index.tsx
+++ react/src/index.tsx
@@ -1,15 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import env from '@ludovicm67/react-dotenv';
+import { OidcProvider } from '@axa-fr/react-oidc';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// fetch values from config
+const publicUrl = env.PUBLIC_URL;
+const gitlabUrl = env.GITLAB;
+const clientId = env.OIDC_CLIENT_ID;
+
+const OIDCConfiguration = {
+  client_id: clientId || '',
+  redirect_uri: `${publicUrl}/oidc/callback`,
+  scope: 'openid profile email read_api read_user api',
+  authority: gitlabUrl || 'https://gitlab.com',
+  authority_configuration: {
+    authorization_endpoint: `${gitlabUrl}/oauth/authorize`,
+    token_endpoint: `${gitlabUrl}/oauth/token`,
+    userinfo_endpoint: `${gitlabUrl}/oauth/userinfo`,
+    revocation_endpoint: `${gitlabUrl}/oauth/revoke`
+  }
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <OidcProvider configuration={OIDCConfiguration}>
+      <App />
+    </OidcProvider>
   </React.StrictMode>
 );
 
