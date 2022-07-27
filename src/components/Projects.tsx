1badd4a14079998128cd475125616988fe2e8117
Ludovic Muller <ludovicmuller1@gmail.com>
2022-07-27 11:00:10 +0200

    filter project by name

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useOidcUser, OidcUserStatus, useOidcAccessToken } from '@axa-fr/react-oidc';
 import env from '@ludovicm67/react-dotenv';
 import { useQuery } from '@tanstack/react-query';
@@ -6,6 +7,7 @@ import { AugmentedProject } from '../lib/gitlabTypes';
 import Project from './Project';
 
 const Projects = () => {
+  const [ filter, setFilter ] = useState<string>('');
   const { accessToken } = useOidcAccessToken();
   const { oidcUserLoadingState } = useOidcUser();
   const selectedTags = `${env.SELECTED_TAGS}`.split(',').map(tag => tag.trim());
@@ -18,6 +20,14 @@ const Projects = () => {
     refetchIntervalInBackground: true,
     refetchOnReconnect: true,
   });
+
+  let filteredProjects = projects.data ?? [];
+  if (filter) {
+    filteredProjects = filteredProjects.filter((project: AugmentedProject) => {
+      return `${project.name_with_namespace}`.toLocaleLowerCase().includes(filter);
+    });
+  }
+
   if (!isLoggedIn) {
     return <div className="container"><p>You are not logged in. Try to refresh the page.</p></div>
   }
@@ -27,9 +37,14 @@ const Projects = () => {
   }
 
   if (projects.isSuccess) {
-    return <div className="container">{projects.data.map((project: AugmentedProject, i: number) => {
-      return <Project key={i} project={project} />
-    })}</div>
+    return <div className="container">
+      <div className="projects-filter">
+        <input type="text" placeholder="Filter projects by name…" onChange={(e) => setFilter(e.target.value.toLocaleLowerCase())} value={filter} />
+      </div>
+      {filteredProjects.map((project: AugmentedProject, i: number) => {
+        return <Project key={i} project={project} />
+      })}
+    </div>
   }
 
   return <div className="container">
